c030ba44db4ebe9eeadb3964c58c24e55c6ee245
Jehan-dev <evgenbuz@inbox.ru>
2019-10-19 16:22:14 +0300

    added site

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $('.menu__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.menu__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.menu__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+})
